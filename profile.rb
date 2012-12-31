e1be6189f4c31e03567b2edaaa23b5e36b053e08
MatmaRex <matma.rex@gmail.com>
2012-12-31 14:53:21 +0100

    profile to a subdir

--- profile.rb
+++ profile.rb
@@ -8,6 +8,10 @@ require_relative 'wikiblame'
 
 $VERBOSE = true
 
+unless File.directory? 'profile'
+	Dir.mkdir 'profile'
+end
+
 # WikiBlame.new lang, article, reverts, collapse, revertshard, pilcrow, parsed, colorusers, granularity
 # blame = WikiBlame.new 'pl', 'Zaginiona flota', true, true, true, false, false, false, 'chars'
 blame = WikiBlame.new 'pl', 'Automatyka domowa', true, true, true, false, false, false, 'chars'
@@ -17,9 +21,9 @@ RubyProf.start
 at_exit {
 	result = RubyProf.stop
 	RubyProf::FlatPrinter.new(result).print(STDOUT)
-	# RubyProf::DotPrinter.new(result).print(f=File.open('graph.dot', 'w')); f.close
-	# system "dot -Tsvg -o graph.svg graph.dot"
-	RubyProf::CallStackPrinter.new(result).print(f=File.open('graph.html', 'w')); f.close
+	# RubyProf::DotPrinter.new(result).print(f=File.open('profile/graph.dot', 'w')); f.close
+	# system "dot -Tsvg -o profile/graph.svg profile/graph.dot"
+	RubyProf::CallStackPrinter.new(result).print(f=File.open('profile/graph.html', 'w')); f.close
 }
 
 
